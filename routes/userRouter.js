87f19ef5d0c2671939b2977bd4b1d24525e137e7
Agustin Gonzalez <agustin.gonzalez.10@hotmail.com>
2024-04-26 10:28:49 -0300

    Adding router

---
+++ routes/userRouter.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', );
+
+router.get('/:id', );
+ 
+router.post('/', );
+
+router.delete('/:id', );
+
+router.put('/:id', );
+
+export default router;
